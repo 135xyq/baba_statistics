72828f59af5dd262b93784bfe06b6b806b43b779
谢永强 <xieyq26@chinaunicom.cn>
2024-09-27 17:25:01 +0800

    每次打开应用获取用户信息

--- App.vue
+++ App.vue
@@ -1,17 +1,42 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+  export default {
+    onLaunch: function() {
+      console.log('App Launch')
+      this.getUserInfo()
+    },
+    onShow: function() {
+      console.log('App Show')
+    },
+    onHide: function() {
+      console.log('App Hide')
+    },
+    methods: {
+     async getUserInfo() {
+        if (this.$store.state.userInfo?.userInfo?.openid) {
+          const state = this.$store.state.userInfo?.userInfo
+          const openid = state.openid
+
+          const msg = await uniCloud.callFunction({
+            name: "get_user_info",
+            data: {
+              openid: openid
+            },
+            success:(res)=> {
+              this.$store.dispatch(
+                "userInfo/updateUserInfo", res.result.data);
+            }
+          });
+        } else {
+          uni.switchTab({
+            url: '/pages/login/login'
+          })
+        }
+      }
+
+    }
+  }
 </script>
 
 <style>
-	/*每个页面公共css */
+  /*每个页面公共css */
 </style>
