295b80404bf24f1674b772a63acbba91c5c8201e
谢永强 <xieyq26@chinaunicom.cn>
2024-10-22 09:09:31 +0800

    位置共享精度确认

--- pages/map/map.vue
+++ pages/map/map.vue
@@ -36,7 +36,7 @@
         this.openid = state.openid
         if (this.openid) {
           uni.getLocation({
-            type: 'wgs84',
+            type: 'gcj02',
             isHighAccuracy: true,
             cacheTimeout: 10,
             success: res => {
@@ -45,13 +45,15 @@
 
               let x = res.longitude
               let y = res.latitude
-              let x_pi = (3.14159265358979324 * 3000.0) / 180.0
-              let z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * x_pi)
-              let theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * x_pi)
-              let lngs = z * Math.cos(theta) + 0.0065
-              let lats = z * Math.sin(theta) + 0.006
+              // let x_pi = (3.14159265358979324 * 3000.0) / 180.0
+              // let z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * x_pi)
+              // let theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * x_pi)
+              // let lngs = z * Math.cos(theta) + 0.0065
+              // let lats = z * Math.sin(theta) + 0.006
 
-              console.log(lats, lngs);
+              let lngs = x
+              let lats = y
+              // console.log(lats, lngs);
               this.latitude = lats
               this.longitude = lngs
 
