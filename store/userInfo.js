cbe4b77a754b731d753592f0b949ea33e57d8331
谢永强 <xieyq26@chinaunicom.cn>
2025-05-23 09:17:37 +0800

    修复获取用户信息失败问题

--- store/userInfo.js
+++ store/userInfo.js
@@ -1,8 +1,8 @@
 export default{
 	namespaced:true,
 	state:{
-		// userInfo:uni.getStorageSync('userInfo') || null,
-		userInfo:{"_id":"66e250572eea65469b5d1da2","nickName":"谢永强","gender":1,"language":"","city":"","province":"","country":"","avatarUrl":"https://mp-47222cf8-47ac-4463-a5d0-2a8b8cb4b608.cdn.bspapp.com/cloudstorage/9e0e57f6-9f1f-4972-8e6b-e073b7350dfd.6893287922557592","is_demote":true,"openid":"oB5r_5SIY6WGY_vYm3n1r6qPWG1M","roleLevel":1},
+		userInfo:uni.getStorageSync('userInfo') || null,
+		// userInfo:{"_id":"66e250572eea65469b5d1da2","nickName":"谢永强","gender":1,"language":"","city":"","province":"","country":"","avatarUrl":"https://mp-47222cf8-47ac-4463-a5d0-2a8b8cb4b608.cdn.bspapp.com/cloudstorage/9e0e57f6-9f1f-4972-8e6b-e073b7350dfd.6893287922557592","is_demote":true,"openid":"oB5r_5SIY6WGY_vYm3n1r6qPWG1M","roleLevel":1},
 	},
 	mutations:{
 		updateUserInfo (state, userInfo) {
